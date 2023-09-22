31eba509db6c4ff278958b6b2e4fee8ef83317ff
spale300 <spalecar1@hotmail.com>
2023-09-22 14:51:44 +0200

    fix

--- src/useMovies.js
+++ src/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setErorr("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}
         `,
             { signal: controller.signal }
           );
